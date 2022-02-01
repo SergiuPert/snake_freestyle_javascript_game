5f6ffaa51dae9494c4427d7d0ae6496f60461dba
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-02-01 11:22:01 +0200

    added navigation

--- server.py
+++ server.py
@@ -10,3 +10,10 @@ app.secret_key = b'_5#87x"F4Qdu\n\xec]/'
 @app.route("/")
 def main_page():
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(
+        port=5000,
+        debug=True
+    )
