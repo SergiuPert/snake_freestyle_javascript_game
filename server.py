91ce7bf8e2405e2c8ebe463462a94f9e9cbee0db
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-02-01 10:11:27 +0200

    added in a python project

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+
+import cryptography
+import database_manager
+
+app = Flask(__name__)
+app.secret_key = b'_5#87x"F4Qdu\n\xec]/'
+
+
+@app.route("/")
+def main_page():
+    return render_template('index.html')
