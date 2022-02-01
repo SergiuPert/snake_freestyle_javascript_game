baad71259b966479c950fb698448873292c897ff
Sidor Andrei <sidor.marian.andrei3001@gmail.com>
2022-02-01 13:56:07 +0200

    added APIs

---
+++ static/script/navigation.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    hideOverlapContent();
+}
+
+function hideOverlapContent() {
+    let overlapDiv = document.getElementById("overlap_div");
+    overlapDiv.style.visibility="hidden"
+}
